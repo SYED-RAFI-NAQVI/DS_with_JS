b4ff66b48295767bc3e339a6398f5b909e4f9390
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2020-04-30 15:24:18 +0530

    added optimize bubble

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,38 @@
+// function bubbleSort(arr){
+//   for(i = arr.length; i > 0; i--){
+//     for(j = 0; j < i - 1; j++){
+//       // console.log(arr, arr[j], arr[j+1])
+//       if(arr[j]> arr[j+1]){
+//         let temp = arr[j]
+//         arr[j] = arr[j+1]
+//         arr[j+1] = temp
+//       }
+//     }
+//   }
+//   return arr
+// }
+//
+// console.log(bubbleSort([23,12,54,2,56,5]))
+
+
+
+// Optimizing Code
 function bubbleSort(arr){
+  let swap
   for(i = arr.length; i > 0; i--){
+    swap = true
     for(j = 0; j < i - 1; j++){
-      console.log(arr, arr[j], arr[j+1])
+      // console.log(arr, arr[j], arr[j+1])
       if(arr[j]> arr[j+1]){
         let temp = arr[j]
         arr[j] = arr[j+1]
         arr[j+1] = temp
+        swap = false
       }
     }
+    if (swap) break
   }
   return arr
 }
 
-console.log(bubbleSort([23,12,54,2,56,5]))
+console.log(bubbleSort([23,1,2,4,5]))
